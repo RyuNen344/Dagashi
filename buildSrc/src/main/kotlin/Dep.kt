d3cd32131431a41cb06e6460df9277636de6e0c0
RyuNen344 <s1100633@outlook.com>
2020-11-29 00:29:54 +0000

    Upgraded androidx.room:room-compiler from $version to 2.2.5

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -38,7 +38,7 @@ object Dep {
         object Room {
             const val version = "2.3.0-alpha03"
             const val core = "androidx.room:room-ktx:$version"
-            const val compiler = "androidx.room:room-compiler:$version"
+            const val compiler = "androidx.room:room-compiler:2.2.5"
         }
 
         object DataStore {
