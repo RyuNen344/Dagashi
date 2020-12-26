34e2a10eec0f4bce8274f81a3177be5c56e58bed
RyuNen344 <s1100633@outlook.com>
2020-12-27 06:04:46 +0900

    設定画面のviewModel接続

--- app/src/main/java/com/ryunen344/dagashi/ui/setting/viewmodel/SettingViewModel.kt
+++ app/src/main/java/com/ryunen344/dagashi/ui/setting/viewmodel/SettingViewModel.kt
@@ -3,15 +3,67 @@ package com.ryunen344.dagashi.ui.setting.viewmodel
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ryunen344.dagashi.data.repository.MileStoneRepository
 import com.ryunen344.dagashi.data.repository.SettingRepository
 import com.ryunen344.dagashi.di.DefaultDispatcher
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class SettingViewModel @ViewModelInject constructor(
     @DefaultDispatcher private val defaultDispatcher: CoroutineDispatcher,
-    private val settingRepository: SettingRepository
+    private val settingRepository: SettingRepository,
+    private val mileStoneRepository: MileStoneRepository
 ) : ViewModel(), SettingViewModelInput, SettingViewModelOutput {
 
     private val viewModelDefaultScope = CoroutineScope(viewModelScope.coroutineContext + defaultDispatcher)
+
+    private val _isOpenInWebView: MutableSharedFlow<Boolean> =
+        MutableSharedFlow(replay = 1, extraBufferCapacity = 0, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+    override val isOpenInWebView: Flow<Boolean>
+        get() = _isOpenInWebView
+
+    init {
+        bindOutput()
+    }
+
+    private fun bindOutput() {
+        settingRepository
+            .isOpenInWebView()
+            .onEach {
+                _isOpenInWebView.emit(it)
+            }.launchIn(viewModelDefaultScope)
+    }
+
+    override fun toggleSettingBrowser(isCurrentChecked: Boolean) {
+        viewModelDefaultScope.launch {
+            runCatching {
+                settingRepository.updateIsOpenInWebView(!isCurrentChecked)
+            }.onFailure {
+                Timber.e(it)
+            }
+        }
+    }
+
+    override fun refreshMileStones() {
+        viewModelDefaultScope.launch {
+            runCatching {
+                mileStoneRepository.refresh()
+            }.onFailure {
+                Timber.e(it)
+            }
+        }
+    }
+
+    override fun onCleared() {
+        viewModelDefaultScope.cancel()
+        super.onCleared()
+    }
 }
--- app/src/main/java/com/ryunen344/dagashi/ui/setting/viewmodel/SettingViewModelInput.kt
+++ app/src/main/java/com/ryunen344/dagashi/ui/setting/viewmodel/SettingViewModelInput.kt
@@ -1,4 +1,6 @@
 package com.ryunen344.dagashi.ui.setting.viewmodel
 
 interface SettingViewModelInput {
+    fun toggleSettingBrowser(isCurrentChecked: Boolean)
+    fun refreshMileStones()
 }
