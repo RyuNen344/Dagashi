9b39957a527fb22d560e43799ccceb14cb5185b6
RyuNen344 <s1100633@outlook.com>
2020-12-27 23:23:00 +0900

    Applicationで初期化する系の処理をInitializerとして切り出し DIで管理するように処理を追加

--- app/src/main/java/com/ryunen344/dagashi/di/AppModule.kt
+++ app/src/main/java/com/ryunen344/dagashi/di/AppModule.kt
@@ -1,9 +1,51 @@
 package com.ryunen344.dagashi.di
 
+import com.ryunen344.dagashi.initializer.AndroidThreeTenInitializer
+import com.ryunen344.dagashi.initializer.CoilInitializer
+import com.ryunen344.dagashi.initializer.EmojiInitializer
+import com.ryunen344.dagashi.initializer.StrictModeInitializer
+import com.ryunen344.dagashi.initializer.TimberInitializer
+import com.ryunen344.dagashi.initializer.core.AppInitializer
+import com.ryunen344.dagashi.initializer.core.AppInitializers
+import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import dagger.multibindings.IntoSet
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideAppInitializers(initializers: Set<@JvmSuppressWildcards AppInitializer>): AppInitializers {
+        return AppInitializers(initializers)
+    }
+
+    @Module
+    @InstallIn(SingletonComponent::class)
+    abstract class AppModuleBinds {
+
+        @Binds
+        @IntoSet
+        abstract fun bindAndroidThreeTenInitializer(impl: AndroidThreeTenInitializer): AppInitializer
+
+        @Binds
+        @IntoSet
+        abstract fun bindCoilInitializer(impl: CoilInitializer): AppInitializer
+
+        @Binds
+        @IntoSet
+        abstract fun bindEmojiInitializer(impl: EmojiInitializer): AppInitializer
+
+        @Binds
+        @IntoSet
+        abstract fun bindStrictModeInitializer(impl: StrictModeInitializer): AppInitializer
+
+        @Binds
+        @IntoSet
+        abstract fun bindTimberInitializer(impl: TimberInitializer): AppInitializer
+    }
+}
---
+++ app/src/main/java/com/ryunen344/dagashi/initializer/core/AppInitializer.kt
@@ -0,0 +1,5 @@
+package com.ryunen344.dagashi.initializer.core
+
+interface AppInitializer {
+    fun initialize()
+}
