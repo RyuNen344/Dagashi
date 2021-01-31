6bb448968cf47b55271f1d56718a042a7dcb873e
RyuNen344 <s1100633@outlook.com>
2021-01-31 06:57:48 +0000

    Upgraded io.ktor:ktor-client-serialization-jvm from $version to 1.4.1

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -95,7 +95,7 @@ object Dep {
     object Ktor {
         private const val version = "1.4.2"
         const val client = "io.ktor:ktor-client-okhttp:1.4.1"
-        const val serialization = "io.ktor:ktor-client-serialization-jvm:$version"
+        const val serialization = "io.ktor:ktor-client-serialization-jvm:1.4.1"
     }
 
     object OkHttp {
