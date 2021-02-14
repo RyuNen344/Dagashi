4636fba7000ba8f09312d36c0b5a6e868d4ace5b
RyuNen344 <s1100633@outlook.com>
2021-02-14 00:31:38 +0000

    Upgraded androidx.lifecycle:lifecycle-common-java8 from 2.2.0 to 2.3.0

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -27,7 +27,7 @@ object Dep {
             private const val version = "2.3.0-rc01"
             const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0"
             const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:2.2.0"
-            const val commonJava8 = "androidx.lifecycle:lifecycle-common-java8:2.2.0"
+            const val commonJava8 = "androidx.lifecycle:lifecycle-common-java8:2.3.0"
         }
 
         object Navigation {
