9ab19302b12768d5b2b0d076c7e00c5163a127fd
RyuNen344 <s1100633@outlook.com>
2021-03-07 00:34:12 +0000

    Upgraded org.jlleitschuh.gradle:ktlint-gradle from $ktlintVersion to 10.0.0

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -9,7 +9,7 @@ object Dep {
         const val releaseHubVersion = "1.6.1"
         const val releaseHub = "com.releaseshub:releases-hub-gradle-plugin:$releaseHubVersion"
         const val ktlintVersion = "10.0.0"
-        const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:$ktlintVersion"
+        const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:10.0.0"
     }
 
     object Android {
