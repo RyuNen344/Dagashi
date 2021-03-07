66719b5fb6d752b5a876bc696ce427d73a86a517
RyuNen344 <s1100633@outlook.com>
2021-03-07 00:34:00 +0000

    Upgraded androidx.lifecycle:lifecycle-livedata-ktx from $version to 2.3.0

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -26,7 +26,7 @@ object Dep {
         object LifeCycle {
             private const val version = "2.3.0"
             const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
-            const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:$version"
+            const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:2.3.0"
             const val commonJava8 = "androidx.lifecycle:lifecycle-common-java8:2.3.0"
         }
 
