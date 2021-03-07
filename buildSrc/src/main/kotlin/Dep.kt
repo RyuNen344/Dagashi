022dba0a330570976ff95c90927e25f58d46f718
RyuNen344 <s1100633@outlook.com>
2021-03-07 00:32:29 +0000

    Upgraded com.squareup.inject:assisted-inject-annotations-dagger2 from $version to 0.6.0

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -78,7 +78,7 @@ object Dep {
 
         object Assisted {
             private const val version = "0.6.0"
-            const val common = "com.squareup.inject:assisted-inject-annotations-dagger2:$version"
+            const val common = "com.squareup.inject:assisted-inject-annotations-dagger2:0.6.0"
             const val compiler = "com.squareup.inject:assisted-inject-processor-dagger2:$version"
         }
     }
