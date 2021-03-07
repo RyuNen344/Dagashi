796e6a83c3bdfc231cb61908d9597b0511191168
RyuNen344 <s1100633@outlook.com>
2021-03-07 00:33:21 +0000

    Upgraded androidx.navigation:navigation-safe-args-gradle-plugin from ${Android.Navigation.version} to 2.3.3

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -2,7 +2,7 @@ object Dep {
 
     object GradlePlugin {
         const val android = "com.android.tools.build:gradle:4.1.2"
-        const val safeArgs = "androidx.navigation:navigation-safe-args-gradle-plugin:${Android.Navigation.version}"
+        const val safeArgs = "androidx.navigation:navigation-safe-args-gradle-plugin:2.3.3"
         const val daggerHilt = "com.google.dagger:hilt-android-gradle-plugin:${Dagger.version}"
         const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}"
         const val kotlinSerialization = "org.jetbrains.kotlin:kotlin-serialization:${Kotlin.version}"
