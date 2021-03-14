feb6c390222dada5496e9b1e58e357874b5462bf
RyuNen344 <s1100633@outlook.com>
2021-03-14 00:33:17 +0000

    Upgraded androidx.navigation:navigation-ui-ktx from 2.3.3 to 2.3.4

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -33,7 +33,7 @@ object Dep {
         object Navigation {
             const val version = "2.3.3"
             const val fragment = "androidx.navigation:navigation-fragment-ktx:2.3.4"
-            const val ui = "androidx.navigation:navigation-ui-ktx:2.3.3"
+            const val ui = "androidx.navigation:navigation-ui-ktx:2.3.4"
         }
 
         object Room {
