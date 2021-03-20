76042e2dd8477b2eac6ca8452448a43053f6bc81
RyuNen344 <s1100633@outlook.com>
2021-03-21 02:58:47 +0900

    Update Dep.kt

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -54,7 +54,6 @@ object Dep {
         const val stdlibJDK8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
 
         object Coroutines {
-            const val version = "1.4.2"
             const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.3"
         }
 
