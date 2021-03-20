3391dbfb8f04138174effb7dc7d8f808887646ec
RyuNen344 <s1100633@outlook.com>
2021-03-21 03:09:33 +0900

    Update Dep.kt

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -31,7 +31,6 @@ object Dep {
         }
 
         object Navigation {
-            const val version = "2.3.3"
             const val fragment = "androidx.navigation:navigation-fragment-ktx:2.3.4"
             const val ui = "androidx.navigation:navigation-ui-ktx:2.3.4"
         }
