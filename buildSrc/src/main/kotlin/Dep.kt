632b5b46ace1d6c97a5109269288999d5ef4c946
RyuNen344 <s1100633@outlook.com>
2021-04-11 00:54:09 +0000

    Upgraded androidx.recyclerview:recyclerview from 1.2.0-alpha06 to 1.2.0

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -20,7 +20,7 @@ object Dep {
         const val material = "com.google.android.material:material:1.3.0"
         const val activity = "androidx.activity:activity-ktx:1.2.2"
         const val fragment = "androidx.fragment:fragment-ktx:1.3.2"
-        const val recycler = "androidx.recyclerview:recyclerview:1.2.0-alpha06"
+        const val recycler = "androidx.recyclerview:recyclerview:1.2.0"
         const val browser = "androidx.browser:browser:1.3.0"
 
         object LifeCycle {
