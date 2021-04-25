41f046b0bea7728fcd958cc895a015e2d5945de6
RyuNen344 <s1100633@outlook.com>
2021-04-25 00:56:44 +0000

    Upgraded androidx.room:room-testing from 2.3.0-beta03 to 2.3.0

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -117,7 +117,7 @@ object Dep {
         object Android {
             const val core = "androidx.test:core-ktx:1.3.0"
             const val junit = "androidx.test.ext:junit-ktx:1.1.2"
-            const val room = "androidx.room:room-testing:2.3.0-beta03"
+            const val room = "androidx.room:room-testing:2.3.0"
             const val espresso = "androidx.test.espresso:espresso-core:3.3.0"
         }
 
