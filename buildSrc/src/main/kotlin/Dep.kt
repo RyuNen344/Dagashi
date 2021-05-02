941c9a295c76c4201e58280fe148a3df0ffa40d4
RyuNen344 <s1100633@outlook.com>
2021-05-02 00:49:50 +0000

    Upgraded org.jetbrains.kotlinx:kotlinx-serialization-json from 1.0.1 to 1.2.0

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -55,7 +55,7 @@ object Dep {
 
         object Serialization {
             const val core = "org.jetbrains.kotlinx:kotlinx-serialization-core:1.2.0"
-            const val json = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1"
+            const val json = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.0"
         }
     }
 
