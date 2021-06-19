bb1b66577f9df2149df60c1cf8fa070b0d95adc9
RyuNen344 <s1100633@outlook.com>
2021-06-19 10:27:58 +0900

    Update Dep.kt

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -8,7 +8,7 @@ object Dep {
         const val kotlinSerialization = "org.jetbrains.kotlin:kotlin-serialization:1.5.10"
         const val releaseHubVersion = "2.0.2"
         const val releaseHub = "com.dipien:releases-hub-gradle-plugin:2.0.2"
-        const val ktlintVersion = "10.0.0"
+        const val ktlintVersion = "10.1.0"
         const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:10.1.0"
     }
 
