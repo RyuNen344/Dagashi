e7168955e3b4fcbde9e51ef2642e896c00286c08
RyuNen344 <s1100633@outlook.com>
2021-09-05 13:15:53 +0900

    Update Dep.kt

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -6,7 +6,7 @@ object Dep {
         const val daggerHilt = "com.google.dagger:hilt-android-gradle-plugin:2.38.1"
         const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30"
         const val kotlinSerialization = "org.jetbrains.kotlin:kotlin-serialization:1.5.30"
-        const val releaseHubVersion = "2.0.2"
+        const val releaseHubVersion = "2.0.3"
         const val releaseHub = "com.dipien:releases-hub-gradle-plugin:2.0.3"
         const val ktlintVersion = "10.1.0"
         const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:10.1.0"
