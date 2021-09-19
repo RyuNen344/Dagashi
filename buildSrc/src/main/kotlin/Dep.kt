d3a25917abcde0a448a62a2fe6c4022bcade31a9
RyuNen344 <s1100633@outlook.com>
2021-09-19 00:08:16 +0000

    Upgraded org.jetbrains.kotlin:kotlin-stdlib-jdk8 from 1.5.30 to 1.5.31

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -47,7 +47,7 @@ object Dep {
 
     object Kotlin {
         const val stdlibCommon = "org.jetbrains.kotlin:kotlin-stdlib:1.5.31"
-        const val stdlibJDK8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.30"
+        const val stdlibJDK8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.31"
 
         object Coroutines {
             const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2"
