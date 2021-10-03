8586950a50a6ef79b737c3e54dd1b9332bb7e3b9
RyuNen344 <s1100633@outlook.com>
2021-10-03 00:09:15 +0000

    Upgraded com.google.dagger:hilt-android from 2.38.1 to 2.39.1

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -60,7 +60,7 @@ object Dep {
     }
 
     object Dagger {
-        const val hilt = "com.google.dagger:hilt-android:2.38.1"
+        const val hilt = "com.google.dagger:hilt-android:2.39.1"
         const val compiler = "com.google.dagger:hilt-android-compiler:2.39.1"
 
         object Android {
