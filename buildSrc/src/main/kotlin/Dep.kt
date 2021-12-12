ebc2d1724c8e5de09007caab4a0c0ec9b774b663
RyuNen344 <s1100633@outlook.com>
2021-12-12 00:11:44 +0000

    Upgraded io.ktor:ktor-client-serialization-jvm from 1.6.6 to 1.6.7

--- buildSrc/src/main/kotlin/Dep.kt
+++ buildSrc/src/main/kotlin/Dep.kt
@@ -78,7 +78,7 @@ object Dep {
 
     object Ktor {
         const val client = "io.ktor:ktor-client-okhttp:1.6.7"
-        const val serialization = "io.ktor:ktor-client-serialization-jvm:1.6.6"
+        const val serialization = "io.ktor:ktor-client-serialization-jvm:1.6.7"
     }
 
     object OkHttp {
